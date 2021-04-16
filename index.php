45a1fbb134fd1968255dd55b92634448d28bbc37
adebayo-adewunmi <adebayo.adewunmi@africaprudential.com>
2021-04-16 15:12:24 +0100

    Initial commit

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
     <head>
         <title>Flick Gateway Checkout Demo</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+        <script src="https://objective-bell-e3e252.netlify.app/checkout.js"></script>
     </head>
     <body>
         <form class="m-5">
